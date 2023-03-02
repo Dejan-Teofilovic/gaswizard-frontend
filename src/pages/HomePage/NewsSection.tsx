1f9f287f3931020b4191498ba44939dd1a30b508
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-03-03 08:01:36 +0900

    NewsSection

--- src/pages/HomePage/NewsSection.tsx
+++ src/pages/HomePage/NewsSection.tsx
@@ -17,17 +17,34 @@ const DATA: Array<string> = [
 
 export default function NewsSection() {
   return (
-    <div className="bg-primary">
-      <div className="container max-w-6xl mx-auto flex flex-col gap-16 py-16">
+    <div className="bg-primary px-6 lg:px-0">
+      <div className="container max-w-6xl mx-auto flex flex-col gap-8 lg:gap-16 py-8 lg:py-16">
         <div className="flex flex-col items-center gap-6">
-          <h2 className="text-4xl font-black text-center text-white uppercase leading-snug">
+          <h2 className="text-2xl lg:text-4xl font-black text-center text-white uppercase leading-snug">
             IN THE NEWS
           </h2>
           <SectionTitleSash1 />
         </div>
         <Swiper
-          slidesPerView={4}
-          spaceBetween={30}
+
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 30
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 30
+            }
+          }}
           pagination={{ clickable: true }}
           modules={[Pagination]}
         >
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <TokenomicsSection />
       <PartnerSection />
       <RoadmapSection />
-      {/* <NewsSection /> */}
+      <NewsSection />
       {/* <FaqSection /> */}
     </div>
   )
