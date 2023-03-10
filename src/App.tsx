fb3a5c23c12f0886f62f7d2d5db25a38916cf2d3
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-03-11 05:41:40 +0900

    remove public provider

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID
 const chains = [bsc]
 
 
-const { provider, webSocketProvider } = configureChains(chains, [w3mProvider({ projectId }), publicProvider()])
+const { provider, webSocketProvider } = configureChains(chains, [w3mProvider({ projectId })])
 
 const wagmiClient = createClient({
   autoConnect: true,
